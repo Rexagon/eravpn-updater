610134b4daa445c167af60a146dd9225e7f7940c
Rexagon <reide740@gmail.com>
2020-01-25 01:54:57 +0300

    Fix authorization routes

--- src/constants.rs
+++ src/constants.rs
@@ -1,2 +1,2 @@
-pub const IGNORE_AUTH: [&str; 3] = ["/api/ping", "/api/signup", "/api/signin"];
+pub const IGNORE_AUTH: [&str; 3] = ["/api/ping", "/api/auth/signup", "/api/auth/signin"];
 pub const AUTHORIZATION_HEADER: &'static str = "Authorization";
